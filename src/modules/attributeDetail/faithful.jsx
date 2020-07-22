e2771f79b4e3b48c5efaddd93ffd79706389c147
bergthold <67835297+bergthold@users.noreply.github.com>
2020-07-22 08:51:23 -0500

    Update faithful.jsx

--- src/modules/attributeDetail/faithful.jsx
+++ src/modules/attributeDetail/faithful.jsx
@@ -29,7 +29,7 @@ class Faithful extends Component {
                   <section>
                   <h1 className="title has-text-info tracking-wide padded">Faithful means able to be trusted. 
                   </h1></section>
-                  <p className="is-size-6 leading-normal padded">We want to be considered trustworthy and are distressed when we find others who we cannot trust.  78-90% of Americans (depending on the poll) believe it is morally wrong to commit an adulterous act.  We desire to be faithful to our spouse, favorite sports team, certain brand or personal habit.  But we also fear that our (and other’s) faithfulness may prove to be faulty - from weakness, fatigue, doubt or a change of will.</p>
+                  <p className="is-size-6 leading-normal padded">We want to be considered trustworthy and are distressed when we find others who we cannot trust.  We desire to be faithful to our favorite sports team, certain brand or personal habit, or to our spouse. 78-90% of Americans (depending on the poll) believe it is morally wrong to commit an adulterous act. But we also fear that our (and other’s) faithfulness may prove to be faulty - from weakness, fatigue, doubt or a change of will.</p>
                   <p className="is-size-6 leading-normal padded">Disappointment, however, seems to be a constant companion to my effort to be faithful and in my attempts to fully trust others.  I work hard to prove myself a reliable person, but it seems a single failure can ruin a hundred successes.  It almost seems like you can’t trust anyone these days!</p>
                 </div>
 
@@ -38,7 +38,7 @@ class Faithful extends Component {
                   <h1 className="title has-text-info tracking-wide padded">How do I avoid being disappointed by others?</h1>
                   </section>
                   <p className="is-size-6 leading-normal padded">All people will eventually disappoint me, including God.  The key question then is who I will trust.  God is one I can trust even if I am disappointed with Him. </p>
-                  <p className="is-size-6 leading-normal padded">After opening with praise and belief in God’s faithful covenant, the psalmist below reveals deep disappointment in God’s apparent lack of faithfulness. His trust in God actually causes his disappointment when circumstances make it seem that a promise may not be kept.  God’s enemies were declaring that no Messiah would ever come (i.e. footsteps), yet the psalmist ends with ‘so be it, I will bless God forever’.  He decided to trust God for what God had not done yet, but promised to do in the future. And the footsteps of Messiah eventually did come!</p>
+                  <p className="is-size-6 leading-normal padded">After opening with praise and belief in God’s faithful covenant, the psalmist below reveals deep disappointment in God’s apparent lack of faithfulness. His trust in God actually causes his disappointment when circumstances make it seem that a promise may not be kept.  God’s enemies were declaring that no Messiah would ever come (i.e. footsteps), yet the psalmist ends with ‘so be it, I will bless God forever.’  He decided to trust God for what God had not done yet, but had promised to do in the future. And the footsteps of Messiah eventually did come!</p>
                   <br></br>
                   <Testimonial quote="But now you have cast off and rejected; you are full of wrath against your anointed. You have renounced the covenant with your servant; you have defiled his crown in the dust…..Lord, where is your steadfast love of old, which by your faithfulness you swore to David? Remember, O Lord, how your servants are mocked, and how I bear in my heart the insults of all the many nations, with which your enemies mock, O LORD, with which they mock the footsteps of your anointed. Blessed be the LORD forever! Amen and Amen." reference="Psalm 89:38-39, 49-52" color="var(--teal)"/>
                 </div>
@@ -48,6 +48,7 @@ class Faithful extends Component {
                   <h1 className="title has-text-info tracking-wide padded">So when should I ever trust another person?</h1>
                 </section>
                   <p className="is-size-6 leading-normal padded">I should not fully trust another who has proven themselves to be unreliable. Trust can be built or rebuilt over time, but I must realize that trusting even the most trust-worthy person to do what only God can do, or provide what only God can give, is a trap.  My fear of being disappointed by others will only be fed when I expect more from them than I do from God.</p>
+                  <p className="is-size-6 leading-normal padded">For example, I want to trust my driver to navigate us through icy mountain roads safely.  He may try his best, but he is not in control of all the circumstances that may happen. It is impossible for him to be fully trustworthy, so fully trusting him could very well end in disappointment.</p>
                   <br></br>
                   <Testimonial quote="Trusting in a treacherous man (i.e. unfaithful, or one who cannot be trusted) in time of trouble is like a bad tooth or a foot that slips." reference="Prov 25:19" color="var(--teal)"
                   quote="It is better to take refuge in the LORD than to trust in man." reference="Psalm 118:8" color="var(--teal)"/>
@@ -73,7 +74,7 @@ class Faithful extends Component {
                 <div className="attSection" id="biblicalChristianity">
                   <section>
                   <h1 className="title has-text-info tracking-wide padded">Jesus was faithful to His Father.</h1></section>
-                  <p className="is-size-6 leading-normal padded">Jesus took on human form and went to the cross as the sacrificial offering which allows for reconciliation of imperfect people to God.  After He completed this work, He joined God in heaven to continue His faithfulness as an advocate, interceding for those who believe in Him.</p>
+                  <p className="is-size-6 leading-normal padded">In obedient faithfulness to His Father, Jesus took on human form and went to the cross as the sacrificial offering which allows for imperfect people to be reconciled to God.  After He completed this work, He joined God in heaven to continue His faithfulness as an advocate, interceding for those who believe in Him.</p>
                   <Testimonial quote="Who is to condemn? Christ Jesus is the one who died—more than that, who was raised—who is at the right hand of God, who indeed is interceding for us." reference="Romans 8:34" color="var(--teal)"/>
                 </div>
 
@@ -81,7 +82,7 @@ class Faithful extends Component {
                 <section>
                   <h1 className="title has-text-info tracking-wide padded">So what do I do now?</h1>
                   </section>
-                  <p className="is-size-6 leading-normal padded">Discover what God has actually promised through study of the Bible.  Then test His reliability and find Him faithful.  Along the way, discover that our ability to be faithful to His commands reliably falls short. This is yet another confirmation that what He has already said is true and that He can be trusted. Ask: How have I discovered or tested God’s faithfulness?</p>
+                  <p className="is-size-6 leading-normal padded">Discover what God has actually promised through study of the Bible.  Then test His reliability and find Him faithful.  Along the way, discover that our ability to be reliably faithful to His commands falls short. This is yet another confirmation that what He has already said is true and that He can be trusted. Ask: How have I discovered or tested God’s faithfulness?</p>
                   <Testimonial quote="The works of his hands are faithful and just; all his precepts are trustworthy;" reference="Psalm 111:7" color="var(--teal)"/>
                 </div>
 
@@ -95,8 +96,9 @@ class Faithful extends Component {
 
                 <div className="attSection has-background-light" id="scienceAndHealth">
                 <section>
-                  <h1 className="title has-text-info tracking-wide padded">God faithfully reveals Himself to us through every part of His creation.We can either seek the One who is the Cause and Faithful Sustainer of all we discover - or we can simply take pride in our own self-sufficiency and power.  The latter appears to be like a baby who just discovered her hands - it can prove to be quite useful, but it is not of ultimate importance.</h1>
+                  <h1 className="title has-text-info tracking-wide padded">God faithfully reveals Himself to us through every part of His creation. </h1>
                   </section>
+                  <p className="is-size-6 leading-normal padded">We can either seek the One who is the Cause and Faithful Sustainer of all we discover - or we can simply take pride in our own self-sufficiency and power.  The latter appears to be like a baby who just discovered her hands - it can prove to be quite useful, but it is not of ultimate importance.</p>
                   <p className="is-size-6 leading-normal padded">“In our time, deistic views (i.e. God started it all and now we are in charge) are influenced by the predominance of science and its technological benefits. Science, it is thought, shows us what the world is like. And the world that it shows us is one in which most things undergo causal developments under their own power. That is, our world is either a world completely without God or a deistic world, in which God mostly leaves the world to its own inner working.</p>
                   <p className="is-size-6 leading-normal padded">But such thinking is a product not of the scientific data, but of analyzing the scientific data in a deistic way. In other words, deism is built into the implicit framework that people assume and use when thinking about science. They interpret the process of causation as self-sufficient, ignoring the presence of God working all things according to his will (Eph. 1:11). They assume self-sufficiency rather than demonstrate it. By contrast, the person who genuinely believes that God is intimately involved in growing grass and making the winds blow sees scientific data as a description of the faithfulness of God. God is so faithful in the ways in which he makes grass grow and the winds blow that we can give detailed descriptions of the regularities. Scientists at their best are merely describing some of the regular ways that God comprehensively rules the world.” - Excerpt from 5/1/19 post by Vern Poythress on Westminster Theological Seminary website, The Faithfulness of God in Science</p>
                   <p className="is-size-6 leading-normal padded">“You cause the grass to grow for the livestock and plants for man to cultivate, that he may bring forth food from the earth.” - Psalm 104:14</p>
@@ -105,23 +107,26 @@ class Faithful extends Component {
 
                 <div className="attSection" id="history">
                 <section>
-                  <h1 className="title has-text-info tracking-wide padded">The key leader was killed on a Roman cross while his few disciples foundered in doubt and mourning.  However, after the resurrection, they were so sure it happened that most risked their very lives based on its truth. The humble beginning has grown into the most global and diverse movement ever.</h1>
+                  <h1 className="title has-text-info tracking-wide padded">The 2000-year global growth of Christianity</h1>
                   </section>
-                  <p className="is-size-6 leading-normal padded">Christianity is 31.5% of global population (with Islam at 23.2% and "Unaffiliated" at 16.3%). This is down from 1900 on a % basis (from 34.8%), but up overall since total population has quadrupled since 1900.  Meanwhile, Christianity has become much more global over that period of time. In 1900, Europe and the Americas had the vast majority of the world’s Christians (93%). Sub-Saharan Africa, Asia and Pacific has seen tremendous growth - in 2010, 26% of the world’s Christians lived in Europe, followed by 24.4% in Latin America and the Caribbean, 23.8% in Sub-Saharan Africa, 13.2% in Asia and the Pacific, 12.3% in North America, and 1% in the Middle East and North Africa. - Data from Wikipedia</p>
+                  <p className="is-size-6 leading-normal padded">The key leader was killed on a Roman cross while his few disciples foundered in doubt and mourning.  However, after the resurrection, they were so sure it happened that it gave them the faith to risk their very lives based on its truth. The humble beginning has grown into the most global and diverse movement ever.</p>
+                  <p className="is-size-6 leading-normal padded">Christianity is 29% of global population (with Islam at 24%, Hindu at 15% and "Unaffiliated/Secular" at 14%). This is down from 1900 on a percentage basis (from 34.8%), but up overall since total population has quadrupled since 1900.  Meanwhile, Christianity has become much more global over that period of time. In 1900, Europe and the Americas had the vast majority of the world’s Christians (93%). Sub-Saharan Africa, Asia and the Pacific has seen tremendous growth - in 2010, 26% of the world’s Christians lived in Europe, followed by 24.4% in Latin America and the Caribbean, 23.8% in Sub-Saharan Africa, 13.2% in Asia and the Pacific, 12.3% in North America, and 1% in the Middle East and North Africa. - Data from Wikipedia 2020</p>
                   
                 </div>
 
                 <div className="attSection" id="liturgy" >
                 <section>
-                    <h1 className="title has-text-info tracking-wide padded">"Steadfast" - Sandra McCracken   (Note the emphasis on trusting in God's faithfulness versus trusting in my faithfulness to God.)</h1>
+                    <h1 className="title has-text-info tracking-wide padded">"Steadfast" - Sandra McCracken   </h1>
                     </section>
-                    <p className="is-size-6 leading-normal padded">"I will build my house
-Whether storm or drought
-On the rock that does not move
-I will set my hope
-In your love, O Lord
-And your faithfulness will prove
-You are steadfast, steadfast"</p>
+                    <p className="is-size-6 leading-normal padded">(Note the emphasis on trusting in God's faithfulness versus trusting in my faithfulness to God.)
+                    
+                    "I will build my house
+                    Whether storm or drought
+                    On the rock that does not move.
+                    I will set my hope
+                    In your love, O Lord
+                    And your faithfulness will prove
+                    You are steadfast, steadfast"</p>
                 </div>
 
             </main>
