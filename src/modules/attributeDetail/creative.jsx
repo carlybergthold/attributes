bfdfc08dea2045f23d5685325fd27e6e767b70a6
Carly Bergthold <carlybergthold@gmail.com>
2020-07-24 12:30:23 -0500

    added imagesand updated sidebar

--- src/modules/attributeDetail/creative.jsx
+++ src/modules/attributeDetail/creative.jsx
@@ -5,6 +5,7 @@ import Grid from '../../components/grid'
 import Testimonial from '../../components/testimonial'
 import Hero from '../../components/hero'
 import AttributeSidebar from "../../components/attSidebar";
+import Quote from '../../components/quote'
 
 class Creative extends Component {
 
@@ -38,10 +39,10 @@ class Creative extends Component {
                   </section>
                   <p className="is-size-6 leading-normal padded">The world delivers so much newness, whether through people I meet or what I wear, drive or eat.  God, our Creator, is the ultimate source behind all this newness.  He also creates in each believer a new spirit which lets me see not only through my own eyes, but His as well - creating an excitement worthy of song!</p>
                   <br></br>
-                  <Testimonial quote="Oh sing to the LORD a new song; sing to the LORD, all the earth!
+                  <Quote scripture="Oh sing to the LORD a new song; sing to the LORD, all the earth!
                   Sing to the LORD, bless his name; tell of his salvation from day to day.
                   Declare his glory among the nations, his marvelous works among all the peoples!
-                  For great is the LORD, and greatly to be praised." reference="Psalm 96:1-6" color="var(--teal)"/>
+                  For great is the LORD, and greatly to be praised." reference="Psalm 96:1-6"/>
                 </div>
 
                 <div className="attSection" id="fear">
@@ -50,7 +51,7 @@ class Creative extends Component {
                 </section>
                   <p className="is-size-6 leading-normal padded">I should first realize that I have already created some kind of legacy (such as what, who and how I love). Then know that an ongoing fear of insignificance always points inward, when much of life is found outside ourselves.  In truth,  my most compelling significance will only be found through knowing and experiencing the One who created me.</p>
                   <br></br>
-                  <Testimonial quote="The end of the matter; all has been heard. Fear God and keep his commandments, for this is the whole duty of man." reference="Eccl 12:13" color="var(--teal)"/>
+                  <Quote scripture="The end of the matter; all has been heard. Fear God and keep his commandments, for this is the whole duty of man." reference="Eccl 12:13" color="var(--teal)"/>
                 </div>
 
                 <div className="attSection" id="struggle">
@@ -59,22 +60,23 @@ class Creative extends Component {
                   </section>
                   <p className="is-size-6 leading-normal padded">I struggle some days to have purpose and fulfillment. The Westminster Catechism describes the Creator’s purpose for our life succinctly: “Man’s chief end is to glorify God, and to enjoy him forever.“ To glorify God, I must first believe in Him, and then begin to know and love Him. And our ultimate joy and feeling of significance in this relationship comes when our will begins to align with the good He has planned for us.</p>
                   <br></br>
-                  <Testimonial quote="For I know the plans I have for you, declares the LORD, plans for welfare and not for evil, to give you a future and a hope." reference="Jer 29:11" color="var(--teal)"/>
+                  <Quote scripture="For I know the plans I have for you, declares the LORD, plans for welfare and not for evil, to give you a future and a hope." reference="Jer 29:11" />
                 </div>
 
                 <div className="attSection" id="whoAmI">
                 <section>
-                  <h1 className="title has-text-primary tracking-wide padded">Only God can create something from absolutely nothing.</h1></section>
+                  <Hero title="Only God can create something from absolutely nothing."></Hero>
+                </section>
                   <p className="is-size-6 leading-normal padded">He created me and all things.  I am created in His image and thus have His desire to create - so my ability, ingredients, and impulse to create come not from myself, but from God.</p>
                   <br></br>
-                  <Testimonial quote="Through him all things were made, without him nothing was made that has been made." reference="John 1:3" color="var(--teal)"/>
+                  <Quote scripture="Through him all things were made, without him nothing was made that has been made." reference="John 1:3" />
                 </div>
 
                 <div className="attSection" id="biblicalChristianity">
                   <section>
                   <h1 className="title has-text-primary tracking-wide padded">Jesus made possible something completely new - to live “in Christ.”</h1></section>
                   <p className="is-size-6 leading-normal padded">Being “in Christ” means I believe that Jesus is actually God who died on the cross to pay the penalty for my sins and, in turn, provides me with His righteousness, allowing me to fellowship with God forever.  With this new relationship comes a completely different perspective to my life and my present circumstances, along with a magnified awareness and value for Creator God.</p>
-                  <Testimonial quote="Therefore, if anyone is in Christ, he is a new creation." reference="2 Cor 5:17" color="var(--teal)"/>
+                  <Quote scripture="Therefore, if anyone is in Christ, he is a new creation." reference="2 Cor 5:17" color="var(--teal)"/>
                 </div>
 
                 <div className="attSection" id="soWhat">
@@ -82,7 +84,7 @@ class Creative extends Component {
                   <h1 className="title has-text-primary tracking-wide padded">So what do I do now?</h1>
                   </section>
                   <p className="is-size-6 leading-normal padded">In humility and gratitude, give credit and appreciation to others who have made it possible for me to create, including our ultimate Creator. Ask: Who has helped me most in forming one of my creative endeavors?</p>
-                  <Testimonial quote="So, whether you eat or drink, or whatever you do, do all to the glory of God." reference="1 Cor 10:31" color="var(--teal)"/>
+                  <Quote scripture="So, whether you eat or drink, or whatever you do, do all to the glory of God." reference="1 Cor 10:31" color="var(--teal)"/>
                 </div>
 
                   <div className="attSection" id="prayer">
@@ -90,7 +92,7 @@ class Creative extends Component {
                   <h1 className="title has-text-primary tracking-wide padded">Prayer</h1>
                   </section>
                   <p className="is-size-6 leading-normal padded">Creator God, thank you for making me a person with creative purpose. Forgive me for thinking I am the ultimate creator of things when I am really your creation. I am creative because I am of your image.  Help me to create to your glory alone.</p>
-                  <Testimonial quote="Through him all things were made, without him nothing was made that has been made." reference="John 1:3" color="var(--teal)"/>
+                  <Quote scripture="Through him all things were made, without him nothing was made that has been made." reference="John 1:3" color="var(--teal)"/>
                 </div>
 
                 <div className="attSection has-background-light" id="scienceAndHealth">
