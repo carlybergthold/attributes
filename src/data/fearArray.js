b104efdaf9d45c6dc961ffcc8b3b6eb3be08d4ad
bergthold <67835297+bergthold@users.noreply.github.com>
2020-07-27 11:03:32 -0500

    Update fearArray.js

--- src/data/fearArray.js
+++ src/data/fearArray.js
@@ -1,131 +1,131 @@
 const fearArray = [
     {
     attributeName: 'creative',
-    fearName: 'insignificance',
+    fearName: 'Insignificance - Am I significant?',
     },
     {
     attributeName: 'eternal',
-    fearName: 'death',
+    fearName: 'Death - I fear dying.',
     },
     {
     attributeName: 'faithful',
-    fearName: 'disappointment in others',
+    fearName: 'dDisappointment from Others - Others will only disappoint me or let me down if I truly rely on them.',
     },
     {
     attributeName: 'glorious',
-    fearName: 'opinion of others',
+    fearName: 'Low Opinion of Others - I fear others may not think well of me.',
     },
     {
     attributeName: 'good',
-    fearName: 'unworthiness',
+    fearName: 'Unworthiness - I am not good enough to/for….',
     },
     {
     attributeName: 'gracious',
-    fearName: 'abuse',
+    fearName: 'Abuse - I fear that, if I let someone off the hook, they may take advantage of me in the future.',
     },
     {
     attributeName: 'holy',
-    fearName: 'unworthy of God',
+    fearName: 'Unworthy of God - I am not good enough to have a relationship with a holy God.',
     },
     {
     attributeName: 'humble',
-    fearName: 'others opinion of me',
+    fearName: 'Wrong Opinions of Me - I need to manage what others think I am.',
     },
     {
     attributeName: 'immutable',
-    fearName: 'loss',
+    fearName: 'Loss - I might lose what I currently enjoy and is important to me.',
     },
     {
     attributeName: 'incomprehensible',
-    fearName: 'full comprehension',
+    fearName: 'Inability to Understand - I may never be able to understand someone or something to my satisfaction.',
     },
     {
     attributeName: 'independent',
-    fearName: 'becoming dependent',
+    fearName: 'Becoming Dependent - I am concerned that I may lose my independence, especially when I get older.',
     },
     {
     attributeName: 'invisible',
-    fearName: 'the hidden',
+    fearName: 'The Hidden - I fear what I cannot see (the dark, the future, germs, death, etc.). ',
     },
     {
     attributeName: 'jealous',
-    fearName: 'punishment',
+    fearName: 'Punishment - I believe God will harm or punish me (as a believer) for ignoring or being unfaithful to Him.',
     },
     {
     attributeName: 'joyful',
-    fearName: 'continued negative emotions',
+    fearName: 'Continued Negative Emotions - I will never be fully happy.',
     },
     {
     attributeName: 'just',
-    fearName: 'being wrong',
+    fearName: 'Being Wrong - I fear being wrong.',
     },
     {
     attributeName: 'loving',
-    fearName: 'being unlovable',
+    fearName: 'Being Unlovable - I will not be loved if I am truly known.',
     },
     {
     attributeName: 'merciful',
-    fearName: 'unworthy of mercy',
+    fearName: 'Unworthy of Mercy - I might not deserve mercy.',
     },
     {
     attributeName: 'omnipotent',
-    fearName: 'failure',
+    fearName: 'Failure - I fear I might not be able to do what I really set out to do.',
     },
     {
     attributeName: 'omnipresent',
-    fearName: 'missing out',
+    fearName: 'Missing Out - I feel like I am missing out on many experiences of life.',
     },
     {
     attributeName: 'omniscient',
-    fearName: 'insufficient information',
+    fearName: 'Insufficient Information - I often can not get enough information to proceed confidently in a  situation.',
     },
     {
     attributeName: 'patient',
-    fearName: 'impending trouble',
+    fearName: 'Impending Trouble - I am concerned what trouble lies ahead.',
     },
     {
     attributeName: 'peaceful',
-    fearName: 'lack of peace',
+    fearName: 'Lack of Peace - I want to maintain peace at all costs.',
     },
     {
     attributeName: 'planning',
-    fearName: 'the future',
+    fearName: 'The Future - I worry that something will happen that I do not like and cannot control.',
     },
     {
     attributeName: 'providing',
-    fearName: 'poverty',
+    fearName: 'Poverty - I worry I may run out of money sometime in my life.',
     },
     {
     attributeName: 'relational',
-    fearName: 'loneliness',
+    fearName: 'Loneliness - Sometimes I think no one really wants to be with me and I might be left alone.',
     },
     {
     attributeName: 'saving',
-    fearName: 'hopelessness',
+    fearName: 'Hopelessness - I feel too far past the point of recovery.',
     },
     {
     attributeName: 'sovereign',
-    fearName: 'losing control',
+    fearName: 'Losing Control - I get concerned when I am not in full control.',
     },
     {
     attributeName: 'teaching',
-    fearName: 'inability to learn',
+    fearName: 'Inability to learn - I will not be able to learn fully what is very important to me.',
     },
     {
     attributeName: 'truthful',
-    fearName: 'deception',
+    fearName: 'Deception - What will happen if I believe a lie?',
     },
     {
     attributeName: 'unique',
-    fearName: 'being average or common',
+    fearName: 'bBeing Average or Common - What if I am just like everyone else?',
     },
     {
     attributeName: 'wise',
-    fearName: 'seen as foolish',
+    fearName: 'Seen As Foolish - Being considered foolish or not able to understand',
     },
     {
     attributeName: 'wrathful',
-    fearName: 'being found out',
+    fearName: 'Being Found Out - My actions may be discovered and I will be punished with restricted freedom.',
     }
 ]
 
