a50fc7ccd171b1ddca4f5a250976712d1f1c73d0
bergthold <67835297+bergthold@users.noreply.github.com>
2020-07-27 11:03:36 -0500

    Update struggleArray.js

--- src/data/struggleArray.js
+++ src/data/struggleArray.js
@@ -1,131 +1,131 @@
 const struggleArray = [
     {
     attributeName: 'creative',
-    struggleName: 'purpose',
+    struggleName: 'Purpose - What is my purpose?',
     },
     {
     attributeName: 'eternal',
-    struggleName: 'pessimism',
+    struggleName: 'Pessimism - I have this sinking feeling that things will not turn out very well in the end. ',
     },
     {
     attributeName: 'faithful',
-    struggleName: 'reliability',
+    struggleName: 'Reliability - I often have a hard time fully meeting the commitments I make.',
     },
     {
     attributeName: 'glorious',
-    struggleName: 'reputation',
+    struggleName: 'Reputation - I want people to think more highly of me.',
     },
     {
     attributeName: 'good',
-    struggleName: 'failure',
+    struggleName: 'Failure - I often do things that disappoint God or other important people in my life.',
     },
     {
     attributeName: 'gracious',
-    struggleName: 'no free lunch',
+    struggleName: 'No Free Lunch - I must work for everything I get.',
     },
     {
     attributeName: 'holy',
-    struggleName: 'making comparisons',
+    struggleName: 'Making Comparisons - My personal holiness certainly suffers at times, but I am as good as most people.',
     },
     {
     attributeName: 'humble',
-    struggleName: 'urgent schedule',
+    struggleName: 'Urgent Schedule - I want to focus more on others but I’ve got a lot going on right now.',
     },
     {
     attributeName: 'immutable',
-    struggleName: 'security',
+    struggleName: 'Security - I need to protect myself and family from unexpected change (through insurance, warranties, precautions, even lawsuit if needed)',
     },
     {
     attributeName: 'incomprehensible',
-    struggleName: 'ability to learn',
+    struggleName: 'Ability to Learn - I really work hard to fully comprehend some things, but my progress seems so slow.',
     },
     {
     attributeName: 'independent',
-    struggleName: 'must do it myself',
+    struggleName: 'Depending on Others - If I depend on someone else, they may fail me.',
     },
     {
     attributeName: 'invisible',
-    struggleName: 'worshipping someone unseen',
+    struggleName: 'Worshipping Someone Unseen - I sometimes have trouble worshiping a god I cannot see.',
     },
     {
     attributeName: 'jealous',
-    struggleName: 'idols',
+    struggleName: 'Idols - People and things I like seem to get in the way of my relationship with God.',
     },
     {
     attributeName: 'joyful',
-    struggleName: 'source of joy',
+    struggleName: 'Source of Joy - I can’t seem to find what brings me joy consistently.',
     },
     {
     attributeName: 'just',
-    struggleName: 'what is fair',
+    struggleName: 'What Is Fair - I often wonder if I am being treated fairly. ',
     },
     {
     attributeName: 'loving',
-    struggleName: 'loving others',
+    struggleName: 'Loving Others - Sometimes I do not feel like loving a specific person.',
     },
     {
     attributeName: 'merciful',
-    struggleName: 'forgiveness',
+    struggleName: 'Forgiveness - I struggle with whether to exercise mercy or justice when dealing with a person who has wronged me.',
     },
     {
     attributeName: 'omnipotent',
-    struggleName: 'with weakness',
+    struggleName: 'With Weakness - I have weaknesses that keep me from doing what I should.',
     },
     {
     attributeName: 'omnipresent',
-    struggleName: 'gods absence',
+    struggleName: 'God's Absence - It seems God is absent.',
     },
     {
     attributeName: 'omniscient',
-    struggleName: 'information overload',
+    struggleName: 'Information Overload - I feel overwhelmed by the amount of knowledge required simply to live these days.ad',
     },
     {
     attributeName: 'patient',
-    struggleName: 'current hardship',
+    struggleName: 'Current Hardship - My current hardship seems like it will never end.',
     },
     {
     attributeName: 'peaceful',
-    struggleName: 'ongoing conflict',
+    struggleName: 'Ongoing Conflict - I am weary from a conflict that just never seems to end.',
     },
     {
     attributeName: 'planning',
-    struggleName: 'perfectionism',
+    struggleName: 'Perfectionism - I like to make a plan and stick to it.',
     },
     {
     attributeName: 'providing',
-    struggleName: 'unmet needs',
+    struggleName: 'Unmet Needs - God must either not know, or not care, about my current unmet need.',
     },
     {
     attributeName: 'relational',
-    struggleName: 'good relationships',
+    struggleName: 'Good Relationships - Relating well to other people can be so hard - I want to do better in this arena.',
     },
     {
     attributeName: 'saving',
-    struggleName: 'codependency',
+    struggleName: 'Codependency - I must fix someone else - save them from drugs, illness, themselves, other danger, etc. without any thought of God.',
     },
     {
     attributeName: 'sovereign',
-    struggleName: 'maintaining control',
+    struggleName: 'Maintaining Control - I have no control over my circumstances or will lose what little control I currently have.',
     },
     {
     attributeName: 'teaching',
-    struggleName: 'difficulty in learning',
+    struggleName: 'Difficulty in Learning - I do not learn well and my teacher will get upset with me.',
     },
     {
     attributeName: 'truthful',
-    struggleName: 'knowing what is true',
+    struggleName: 'Knowing What is True - How do I know what is truly right?',
     },
     {
     attributeName: 'unique',
-    struggleName: 'acceptance',
+    struggleName: 'Acceptance - My uniquenesses will not be accepted by others - or worse, will be made fun of or derided.',
     },
     {
     attributeName: 'wise',
-    struggleName: 'keeping perspective',
+    struggleName: 'Keeping Perspective - I lose perspective by being too short-sighted, tired, selfish, one-sided, etc.',
     },
     {
     attributeName: 'wrathful',
-    struggleName: 'obedience',
+    struggleName: 'Obedience - That I cannot obey perfectly and that my human faults will be exposed (such as what drives a fear of public speaking).',
     }
 ]
 
