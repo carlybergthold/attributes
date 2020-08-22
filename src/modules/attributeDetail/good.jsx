712d3e384263d462502776f35f74f5576e59536c
bergthold <67835297+bergthold@users.noreply.github.com>
2020-08-22 14:08:32 -0500

    Update good.jsx

--- src/modules/attributeDetail/good.jsx
+++ src/modules/attributeDetail/good.jsx
@@ -115,13 +115,13 @@ class Good extends Component {
                 <section>
                     <h1 className="title has-text-primary tracking-wide padded">"Come Thou Fount of Every Blessing" - Robert Robison (Our need is to "tune our hearts" to what is truly good.)</h1>
                     </section>
-                    <p className="is-size-6 leading-normal padded">"Come, Thou Fount of every blessing
-Tune my heart to sing Thy grace
-Streams of mercy, never ceasing
-Call for songs of loudest praise
-Teach me some melodious sonnet
-Sung by flaming tongues above
-Praise the mount, I'm fixed upon it
+                    <p className="is-size-6 leading-normal padded">"Come, Thou Fount of every blessing<br></br>
+Tune my heart to sing Thy grace<br></br>
+Streams of mercy, never ceasing<br></br>
+Call for songs of loudest praise<br></br>
+Teach me some melodious sonnet<br></br>
+Sung by flaming tongues above<br></br>
+Praise the mount, I'm fixed upon it<br></br>
 Mount of Thy unchanging love" - 1st verse</p>
                 </div>
 
