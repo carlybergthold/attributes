8c0ca6c52be3dad60a99d1b8867145e8d54e70c3
bergthold <67835297+bergthold@users.noreply.github.com>
2020-08-29 10:14:38 -0500

    Update omniscient.jsx

--- src/modules/attributeDetail/omniscient.jsx
+++ src/modules/attributeDetail/omniscient.jsx
@@ -83,7 +83,7 @@ class Omniscient extends Component {
                 <section>
                   <h1 className="title has-text-info tracking-wide padded">So what do I do now?</h1>
                   </section>
-                  <p className="is-size-6 leading-normal padded">Pray - in full confidence that I will not only be heard, but fully understood, and with a response that considers all the options and other factors of which I am currently clueless.  Let my confession become more self-admittance and begin my true learning with God.  Ask: How do I pray if God truly knows all?</p>
+                  <p className="is-size-6 leading-normal padded">Pray - in full confidence that I will not only be heard, but fully understood, and with a response that considers all the options and other factors of which I am currently clueless.  Let my confession become more self-admittance and begin my true learning with God.  <br></br>Ask: How do I pray if God truly knows all?</p>
                   <Testimonial quote="Before they call I will answer; while they are yet speaking I will hear." reference="Isaiah 65:24" color="var(--teal)"/>
                 </div>
 
