9a54a9043a18ddcaa121c5e1217aa32ed32ddd96
bergthold <67835297+bergthold@users.noreply.github.com>
2020-12-01 12:03:14 -0600

    Update myersBriggsArray.js

--- src/data/myersBriggsArray.js
+++ src/data/myersBriggsArray.js
@@ -2,82 +2,82 @@ const myersBriggsArray = [
     {
     number: 1,
     type: 'ISTJ',
-    description: 'Quiet, serious, earn success by thoroughness and dependability. Practical, matter-of-fact, realistic, and responsible. Decide logically what should be done and work toward it steadily, regardless of distractions. Take pleasure in making everything orderly and organized - their work, their home, their life. Value traditions and loyalty.'
+    description: 'I am quiet, serious, earn success by thoroughness and dependability. Practical, matter-of-fact, realistic, and responsible. Decide logically what should be done and work toward it steadily, regardless of distractions. Take pleasure in making everything orderly and organized - my work, my home, my life. Value traditions and loyalty.'
     },
     {
     number: 2,
     type: 'ISTP',
-    description: 'Tolerant and flexible, quiet observers until a problem appears, then act quickly to find workable solutions. Analyze what makes things work and readily get through large amounts of data to isolate the core of practical problems. Interested in cause and effect, organize facts using logical principles, value efficiency.'
+    description: 'I am tolerant and flexible, a quiet observer until a problem appears, then act quickly to find workable solutions. Analyze what makes things work and readily get through large amounts of data to isolate the core of practical problems. Interested in cause and effect, organize facts using logical principles, value efficiency.'
     },
     {
     number: 3,
     type: 'ISFJ',
-    description: 'Quiet, friendly, responsible, and conscientious. Committed and steady in meeting their obligations. Thorough, painstaking, and accurate. Loyal, considerate, notice and remember specifics about people who are important to them, concerned with how others feel. Strive to create an orderly and harmonious environment at work and at home.'
+    description: 'I am quiet, friendly, responsible, and conscientious. Committed and steady in meeting my obligations. Thorough, painstaking, and accurate. Loyal, considerate, notice and remember specifics about people who are important to me, concerned with how others feel. Strive to create an orderly and harmonious environment at work and at home.'
     },
     {
     number: 4,
     type: 'ISFP',
-    description: 'Quiet, friendly, sensitive, and kind. Enjoy the present moment and what is going on around them. Like to have their own space and to work within their own time frame. Loyal and committed to their values and to people who are important to them. Dislike disagreements and conflicts, do not force their opinions or values on others.'
+    description: 'I am quiet, friendly, sensitive, and kind. Enjoy the present moment and what is going on around me. Like to have my own space and to work within my own time frame. Loyal and committed to my values and to people who are important to me. Dislike disagreements and conflicts, do not force my opinions or values on others.'
     },
     {
     number: 5,
     type: 'INTJ',
-    description: 'Have original minds and great drive for implementing their ideas and achieving their goals. Quickly see patterns in external events and develop long-range explanatory perspectives. When committed, organize a job and carry it through. Skeptical and independent, have high standards of competence and performance - for themselves and others.'
+    description: 'I have an original mind and great drive for implementing my ideas and achieving my goals. Quickly see patterns in external events and develop long-range explanatory perspectives. When committed, organize a job and carry it through. Skeptical and independent, have high standards of competence and performance - for myself and others.'
     },
     {
     number: 6,
     type: 'INTP',
-    description: 'Seek to develop logical explanations for everything that interests them. Theoretical and abstract, interested more in ideas than in social interaction. Quiet, contained, flexible, and adaptable. Have unusual ability to focus in depth to solve problems in their area of interest. Skeptical, sometimes critical, always analytical.'
+    description: 'I seek to develop logical explanations for everything that interests me. Theoretical and abstract, interested more in ideas than in social interaction. Quiet, contained, flexible, and adaptable. Have unusual ability to focus in depth to solve problems in my area of interest. Skeptical, sometimes critical, always analytical.'
     },
     {
     number: 7,
     type: 'ESTJ',
-    description: 'Practical, realistic, matter-of-fact. Decisive, quickly move to implement decisions. Organize projects and people to get things done, focus on getting results in the most efficient way possible. Take care of routine details. Have a clear set of logical standards, systematically follow them and want others to also. Forceful in implementing their plans.'
+    description: 'I am practical, realistic, matter-of-fact. Decisive, quickly move to implement decisions. Organize projects and people to get things done, focus on getting results in the most efficient way possible. Take care of routine details. Have a clear set of logical standards, systematically follow them and want others to also. Forceful in implementing my plans.'
     },
     {
     number: 8,
     type: 'ESTP',
-    description: 'Flexible and tolerant, they take a pragmatic approach focused on immediate results. Theories and conceptual explanations bore them - they want to act energetically to solve the problem. Focus on the here-and-now, spontaneous, enjoy each moment that they can be active with others. Enjoy material comforts and style. Learn best through doing.'
+    description: 'I am flexible and tolerant, taking a pragmatic approach focused on immediate results. Theories and conceptual explanations bore me - I want to act energetically to solve the problem. Focus on the here-and-now, spontaneous, enjoy each moment that I can be active with others. Enjoy material comforts and style. Learn best through doing.'
     },
     {
     number: 9,
     type: 'ESFJ',
-    description: 'Warmhearted, conscientious, and cooperative. Want harmony in their environment, work with determination to establish it. Like to work with others to complete tasks accurately and on time. Loyal, follow through even in small matters. Notice what others need in their day-by-day lives and try to provide it. Want to be appreciated for who they are and for what they contribute.'
+    description: 'I am warmhearted, conscientious, and cooperative. Want harmony in my environment, work with determination to establish it. Like to work with others to complete tasks accurately and on time. Loyal, follow through even in small matters. Notice what others need in their day-by-day lives and try to provide it. Want to be appreciated for who I am and for what I contribute.'
     },
     {
     number: 10,
     type: 'ESFP',
-    description: 'Outgoing, friendly, and accepting. Exuberant lovers of life, people, and material comforts. Enjoy working with others to make things happen. Bring common sense and a realistic approach to their work, and make work fun. Flexible and spontaneous, adapt readily to new people and environments. Learn best by trying a new skill with other people.'
+    description: 'I am outgoing, friendly, and accepting. Exuberant lover of life, people, and material comforts. Enjoy working with others to make things happen. Bring common sense and a realistic approach to my work, and make work fun. Flexible and spontaneous, adapt readily to new people and environments. Learn best by trying a new skill with other people.'
     },
     {
     number: 11,
     type: 'ENTJ',
-    description: 'Frank, decisive, assume leadership readily. Quickly see illogical and inefficient procedures and policies, develop and implement comprehensive systems to solve organizational problems. Enjoy long-term planning and goal setting. Usually well informed, well read, enjoy expanding their knowledge and passing it on to others. Forceful in presenting their ideas.'
+    description: 'I am frank, decisive, assume leadership readily. Quickly see illogical and inefficient procedures and policies, develop and implement comprehensive systems to solve organizational problems. Enjoy long-term planning and goal setting. Usually well informed, well read, enjoy expanding my knowledge and passing it on to others. Forceful in presenting my ideas.'
     },
     {
     number: 12,
     type: 'ENTP',
-    description: 'Quick, ingenious, stimulating, alert, and outspoken. Resourceful in solving new and challenging problems. Adept at generating conceptual possibilities and then analyzing them strategically. Good at reading other people. Bored by routine, will seldom do the same thing the same way, apt to turn to one new interest after another.'
+    description: 'I am quick, ingenious, stimulating, alert, and outspoken. Resourceful in solving new and challenging problems. Adept at generating conceptual possibilities and then analyzing them strategically. Good at reading other people. Bored by routine, will seldom do the same thing the same way, apt to turn to one new interest after another.'
     },
     {
     number: 13,
     type: 'INFJ',
-    description: 'Seek meaning and connection in ideas, relationships, and material possessions. Want to understand what motivates people and are insightful about others. Conscientious and committed to their firm values. Develop a clear vision about how best to serve the common good. Organized and decisive in implementing their vision.'
+    description: 'I seek meaning and connection in ideas, relationships, and material possessions. Want to understand what motivates people and am insightful about others. Conscientious and committed to my firm values. Develop a clear vision about how best to serve the common good. Organized and decisive in implementing my vision.'
     },
     {
     number: 14,
     type: 'INFP',
-    description: 'Idealistic, loyal to their values and to people who are important to them. Want an external life that is congruent with their values. Curious, quick to see possibilities, can be catalysts for implementing ideas. Seek to understand people and to help them fulfill their potential. Adaptable, flexible, and accepting unless a value is threatened.'
+    description: 'I am idealistic, loyal to my values and to people who are important to me. Want an external life that is congruent with my values. Curious, quick to see possibilities, can be catalysts for implementing ideas. Seek to understand people and to help me fulfill my potential. Adaptable, flexible, and accepting unless a value is threatened.'
     },
     {
     number: 15,
     type: 'ENFJ',
-    description: 'Warm, empathetic, responsive, and responsible. Highly attuned to the emotions, needs, and motivations of others. Find potential in everyone, want to help others fulfill their potential. May act as catalysts for individual and group growth. Loyal, responsive to praise and criticism. Sociable, facilitate others in a group, and provide inspiring leadership.'
+    description: 'I am warm, empathetic, responsive, and responsible. Highly attuned to the emotions, needs, and motivations of others. Find potential in everyone, want to help others fulfill their potential. May act as catalysts for individual and group growth. Loyal, responsive to praise and criticism. Sociable, facilitate others in a group, and provide inspiring leadership.'
     },
     {
     number: 16,
     type: 'ENFP',
-    description: 'Warmly enthusiastic and imaginative. See life as full of possibilities. Make connections between events and information very quickly, and confidently proceed based on the patterns they see. Want a lot of affirmation from others, and readily give appreciation and support. Spontaneous and flexible, often rely on their ability to improvise and their verbal fluency.'
+    description: 'I am warmly enthusiastic and imaginative. See life as full of possibilities. Make connections between events and information very quickly, and confidently proceed based on the patterns I see. Want a lot of affirmation from others, and readily give appreciation and support. Spontaneous and flexible, often rely on my ability to improvise and my verbal fluency.'
     }
 ]
 
