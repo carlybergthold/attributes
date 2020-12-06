bdfb206ab8eb9baca0262d513877d32542b875c9
bergthold <67835297+bergthold@users.noreply.github.com>
2020-12-06 11:24:54 -0600

    Update fears.jsx

--- src/components/connectors/fears.jsx
+++ src/components/connectors/fears.jsx
@@ -18,7 +18,7 @@ class Fears extends Component {
                 <div className="section">
                     <div className="container">
                         <h1>What is your biggest fear today?</h1>
-                        <Grouping connector="emotion"/>
+                        <Grouping />
                     </div>
                 </div>
             </div>
