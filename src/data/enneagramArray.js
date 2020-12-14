29833599375dbc3cc0386faf90f3c99a66185275
bergthold <67835297+bergthold@users.noreply.github.com>
2020-12-14 16:37:03 -0600

    Update enneagramArray.js

--- src/data/enneagramArray.js
+++ src/data/enneagramArray.js
@@ -3,54 +3,63 @@ const enneagramArray = [
     number: 1,
     type: '1 - Reformer',
     description: 'I am the Rational, Idealistic Type: Principled, Purposeful, Self-Controlled, and Perfectionistic',
+    attributeDescription: 'So God and I both like to be Just!',
     attribute: 'Just'
     },
     {
     number: 2,
     type: '2 - Helper',
     description: 'I am the Caring, Interpersonal Type: Demonstrative, Generous, People-Pleasing, and Possessive',
+    attributeDescription: 'So God and I both like to be Loving!',
     attribute: 'Loving'
     },
     {
     number: 3,
     type: '3 - Achiever',
     description: 'I am the Success-Oriented, Pragmatic Type: Adaptive, Excelling, Driven, and Image-Conscious',
+    attributeDescription: 'So God and I both like to be Sovereign!',
     attribute: 'Sovereign'
     },
     {
     number: 4,
     type: '4 - Individualist',
     description: 'I am the Sensitive, Withdrawn Type: Expressive, Dramatic, Self-Absorbed, and Temperamental',
+    attributeDescription: 'So God and I both like to be Independent!',
     attribute: 'Independent'
     },
     {
     number: 5,
     type: '5 - Investigator',
     description: 'I am the Intense, Cerebral Type: Perceptive, Innovative, Secretive, and Isolated',
+    attributeDescription: 'So God and I both like to be Omniscient!',
     attribute: 'Omniscient'
     },
     {
     number: 6,
     type: '6 - Loyalist',
     description: 'I am the Committed, Security-Oriented Type: Engaging, Responsible, Anxious, and Suspicious',
+    attributeDescription: 'So God and I both like to be Faithful!',
     attribute: 'Faithful'
     },
     {
     number: 7,
     type: '7 - Enthusiast',
     description: 'I am the Busy, Fun-Loving Type: Spontaneous, Versatile, Distractible, and Scattered',
+    attributeDescription: 'So God and I both like to be Omnipresent!',
     attribute: 'Omnipresent'
     },
     {
     number: 8,
     type: '8 - Challenger',
     description: 'I am the Powerful, Dominating Type: Self-Confident, Decisive, Willful, and Confrontational',
+    attributeDescription: 'So God and I both like to be Omnipotent!',
     attribute: 'Omnipotent'
     },
     {
     number: 9,
     type: '9 - Peacemaker',
     description: 'I am the Easygoing, Self-Effacing Type: Receptive, Reassuring, Agreeable, and Complacent',
+    attributeDescription: 'So God and I both like to be Peaceful!',
     attribute: 'Peaceful'
     }
 ]
