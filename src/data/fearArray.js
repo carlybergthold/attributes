f1d40aa466c8743eaa851dc7230df517ea6b6584
bergthold <67835297+bergthold@users.noreply.github.com>
2020-12-14 16:36:59 -0600

    Update fearArray.js

--- src/data/fearArray.js
+++ src/data/fearArray.js
@@ -2,193 +2,193 @@ const fearArray = [
     {
     attributeName: 'creative',
     fearName: 'Insignificance',
-    fearDescription: 'Am I significant?',
+    fearDescription: 'When I fear that I am insignificant, I need to seek the one who Created me.',
     group: 'Shame'
     },
     {
     attributeName: 'eternal',
     fearName: 'Death',
-    fearDescription: 'I fear dying.',
+    fearDescription: 'When I fear death, I need to seek a longer perspective of an Eternal God.',
     group: 'Survival'
     },
     {
     attributeName: 'faithful',
     fearName: 'Disappointment from others',
-    fearDescription: 'Others will let me down if I truly rely on them.',
+    fearDescription: 'When I fear that others will let me down if I truly rely on them, I need to seek a Faithful God.',
     group: 'Loss of Control'
     },
     {
     attributeName: 'glorious',
     fearName: 'Low opinion of others',    
-    fearDescription: 'I fear others may not think well of me.',
+    fearDescription: 'When I fear what others think of me, I will do well to ponder the Glory of God.',
     group: 'Rejection'
     },
     {
     attributeName: 'good',
     fearName: 'Unworthiness',
-    fearDescription: 'I am not good enough to/for….',
+    fearDescription: 'When I fear I will not be good enough, I can rest in the Goodness of God.',
     group: 'Rejection'
     },
     {
     attributeName: 'gracious',
     fearName: 'Abuse',
-    fearDescription: 'I fear that others may take advantage of me.',
+    fearDescription: 'When I fear that others may take advantage of me, I need to explore the Graciousness of God.',
     group: 'Survival'
     },
     {
     attributeName: 'holy',
     fearName: 'Unworthy of God',
-        fearDescription: 'I am not worthy of a holy God.',
+        fearDescription: 'When I fear that I am not worthy of a Holy God, I am on a good path.',
 group: 'Rejection'
     },
     {
     attributeName: 'humble',
     fearName: 'Wrong opinions of me',
-        fearDescription: 'I need to manage what others think I am.',
+        fearDescription: 'When I fear what others think of me, I need to understand the Humility of God.',
 group: 'Rejection'
     },
     {
     attributeName: 'immutable',
     fearName: 'Loss',
-        fearDescription: 'I might lose what is important to me.',
+        fearDescription: 'When I fear losing what is important to me, I need to stand firm in the Immutability of God.',
 group: 'Loss of control'
     },
     {
     attributeName: 'incomprehensible',
     fearName: 'Inability to understand',
-        fearDescription: 'I may never understand something to my satisfaction.',
+        fearDescription: 'When I fear I may never fully understanding something, I need to trust an Incomprehensible God.',
 group: 'Loss of control'
     },
     {
     attributeName: 'independent',
     fearName: 'Becoming dependent',
-        fearDescription: 'I fear I may lose my independence.',
+        fearDescription: 'When I fear I may lose my independence, I need to depend on an Independent God.',
 group: 'Loss of control'
     },
     {
     attributeName: 'invisible',
     fearName: 'The hidden',
-        fearDescription: 'I fear what I cannot see.',
+        fearDescription: 'When I fear what I cannot see, I need to consider the reality of an Invisible God.',
 group: 'Survival'
     },
     {
     attributeName: 'jealous',
     fearName: 'Punishment',
-        fearDescription: 'I believe God will harm or punish me.',
+        fearDescription: 'When I fear that God will harm or punish me, I need to understand the Jealousy of God.',
 group: 'Rejection'
     },
     {
     attributeName: 'joyful',
-    fearName: 'Continued negative emotions',
-        fearDescription: 'I will never be fully happy.',
+    fearName: 'Continued unhappiness',
+        fearDescription: 'When I fear I may never be truly happy, I need to explore the Joy of God.',
 group: 'Loss of control'
     },
     {
     attributeName: 'just',
     fearName: 'Being wrong',
-        fearDescription: 'I fear being wrong.',
+        fearDescription: 'When I fear being wrong, I need to dwell on the Justice of God.',
 group: 'Shame'
     },
     {
     attributeName: 'loving',
     fearName: 'Being unlovable',
-        fearDescription: 'I will not be loved if I am truly known.',
+        fearDescription: 'When I fear I will not be loved if I am truly known, I need to talk with the God of Love.',
 group: 'Shame'
     },
     {
     attributeName: 'merciful',
     fearName: 'Unworthy of mercy',
-        fearDescription: 'I might not deserve mercy.',
+        fearDescription: 'When I fear that I might not deserve mercy, I need to experience a Merciful God.',
 group: 'Shame'
     },
     {
     attributeName: 'omnipotent',
     fearName: 'Failure',
-        fearDescription: 'I fear I might fail.',
+        fearDescription: 'When I fear I might fail, I need to trust in an Omnipotent God.',
 group: 'Loss of control'
     },
     {
     attributeName: 'omnipresent',
     fearName: 'Missing out',
-        fearDescription: 'I am missing out on much of life.  ',
+        fearDescription: 'When I fear I am missing out on much of life, I need to appreciate an Omnipresent God.',
 group: 'Loss of control'
     },
     {
     attributeName: 'omniscient',
     fearName: 'Insufficient information',
-        fearDescription: 'I cannot get enough info to proceed.',
+        fearDescription: 'When I cannot get enough info to move forward, I need to seek guidance from an Omniscient God.',
 group: 'Shame'
     },
     {
     attributeName: 'patient',
     fearName: 'Impending trouble',
-        fearDescription: 'I see trouble ahead!',
+        fearDescription: 'When I fear the trouble ahead, I need to learn from a Patient God.',
 group: 'Survival'
     },
     {
     attributeName: 'peaceful',
-    fearName: 'Lack of peace',
-        fearDescription: 'I want peace at all costs.',
+    fearName: 'Never ending trouble',
+        fearDescription: 'When I fear that my troubles will never end, I need to see the Peace of God.',
 group: 'Loss of control'
     },
     {
     attributeName: 'planning',
     fearName: 'The future',
-        fearDescription: 'I cannot keep everything in control.',
+        fearDescription: 'When I fear that I cannot control my future, I need to meet a God with a Plan.',
 group: 'Loss of control'
     },
     {
     attributeName: 'providing',
     fearName: 'Poverty',
-        fearDescription: 'I might eventually run out of money.',
+        fearDescription: 'When I fear that I might eventually run out of money, I need to trust a Providing God.',
 group: 'Survival'
     },
     {
     attributeName: 'relational',
     fearName: 'Loneliness',
-        fearDescription: 'No one wants to be with me.',
+        fearDescription: 'When I fear that no one ever wants to be with me, I need to see how Relational God is.',
 group: 'Rejection'
     },
     {
     attributeName: 'saving',
     fearName: 'Hopelessness',
-        fearDescription: 'I am past hope.',
+        fearDescription: 'When I fear that I am hopeless, I need to turn to the God who Saves.',
 group: 'Survival'
     },
     {
     attributeName: 'sovereign',
     fearName: 'Losing control',
-        fearDescription: 'I really need to stay in control.',
+        fearDescription: 'When I fear that I am losing control, I need to trust in Sovereign God.',
 group: 'Loss of control'
     },
     {
     attributeName: 'teaching',
     fearName: 'Inability to learn',
-        fearDescription: 'I will not be able to learn all I need.',
+        fearDescription: 'When I fear that I will not be able to learn all I need to learn, I need to approach Teacher God.',
 group: 'Survival'
     },
     {
     attributeName: 'truthful',
     fearName: 'Deception',
-       fearDescription: 'Am I believing a lie?',
+       fearDescription: 'When I fear that I am believing a lie, I need to go to the God of Truth.',
  group: 'Survival'
     },
     {
     attributeName: 'unique',
     fearName: 'Being average or common',
-    fearDescription: 'What if I am just like everyone else?',
+    fearDescription: 'When I fear that I am just like everyone else, I need to learn from a Unique God.',
     group: 'Shame'
     },
     {
     attributeName: 'wise',
     fearName: 'Seen as foolish',
-    fearDescription: 'I might seem foolish.',
+    fearDescription: 'When I fear that I might seem foolish, I need to approach the God of Wisdom.',
     group: 'Shame'
     },
     {
     attributeName: 'wrathful',
     fearName: 'Being found out',
-    fearDescription: 'I might be found out and suffer consequences.',
+    fearDescription: 'When I fear that I might be found out and suffer consequences, I need to better understand the God of Wrath.',
     group: 'Rejection'
     }
 ]
