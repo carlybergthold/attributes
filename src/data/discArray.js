dbe81f47f2c6b2aae8031097c4f55b21da8459f3
bergthold <67835297+bergthold@users.noreply.github.com>
2020-12-14 16:37:09 -0600

    Update discArray.js

--- src/data/discArray.js
+++ src/data/discArray.js
@@ -3,24 +3,28 @@ const discArray = [
     number: 1,
     type: 'Dominance',
     description: 'I am direct, strong-willed and forceful',
+    attributeDescription: 'So I reflect and desire the Omnipotence of God!',
     attribute: 'Omnipotent'
     },
     {
     number: 2,
     type: 'Influence',
     description: 'I am sociable, talkative and lively',
+    attributeDescription: 'So I reflect and desire the Relational strength of God!',
     attribute: 'Relational'
     },
     {
     number: 3,
     type: 'Steadiness',
     description: 'I am gentle, accommodating and soft-hearted',
+    attributeDescription: 'So I reflect and desire the Peace of God!',
     attribute: 'Peaceful'
     },
     {
     number: 4,
     type: 'Conscientiousness',
     description: 'I am private, analytical and logical',
+    attributeDescription: 'So I reflect and desire the Wisdom of God!',
     attribute: 'Wise'
     }
 ]
