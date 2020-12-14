59d0075cd246a91444cb075f27c85e57e562c44a
bergthold <67835297+bergthold@users.noreply.github.com>
2020-12-14 16:37:06 -0600

    Update emotionArray.js

--- src/data/emotionArray.js
+++ src/data/emotionArray.js
@@ -2,185 +2,185 @@ const emotionArray = [
     {
     attributeName: 'creative',
     emotionName: 'Excitement',
-    emotionDescription: 'Great enthusiasm and eagerness',
+    emotionConnectDescription: 'Excitement often comes from what is new - and God is the Creator of all new things!',
     group: 'pleasure'
     },
     {
     attributeName: 'eternal',
     emotionName: 'Grief',
-    emotionDescription: 'Deep sorrow, especially caused by a death',
+    emotionConnectDescription: 'Grief is from suffering a loss or death - but God is Eternal and always remains to comfort my grief.',
     group: 'sadness'
     },
     {
     attributeName: 'faithful',
     emotionName: 'Disappointment',
-    emotionDescription: 'Sadness or displeasure from nonfulfillment of expectations',
+    emotionConnectDescription: 'Disappointment is from unmet expectations - but God is Faithful to do all that He has promised.',
     group: 'sadness'
     },
     {
     attributeName: 'glorious',
     emotionName: 'Rejection',
-    emotionDescription: 'The dismissing or refusing of an idea or affection',
+    emotionConnectDescription: 'Rejection of who I am or what I offer hurts - but I am hurt even more when I reject God and His Glory.',
     group: 'fear'
     },
     {
     attributeName: 'good',
     emotionName: 'Satisfaction',
-    emotionDescription: 'The feeling gained from fulfillment of a need or desire',
+    emotionConnectDescription: 'Satisfaction means my need or desire was met - and all my needs and desires ultimately come from God and are thus Good.',
     group: 'pleasure'
     },
     {
     attributeName: 'gracious',
     emotionName: 'Love',
-    emotionDescription: 'An intense feeling of deep affection',
+    emotionConnectDescription: 'True love is more gift than emotion - and God is Gracious in giving us His love first.',
     group: 'pleasure'
 },
     {
     attributeName: 'holy',
     emotionName: 'Guilt',
-    emotionDescription: 'A feeling of having done wrong or failed in an obligation',
+    emotionConnectDescription: 'Guilt comes from doing wrong - and a Holy God always brings guilt in some form to all people.',
     group: 'fear'
 },
     {
     attributeName: 'humble',
     emotionName: 'Anger',
-    emotionDescription: 'A strong feeling of annoyance, displeasure, or hostility',
+    emotionConnectDescription: 'Anger often seeks flawed justice or punishment - but God tempers anger with Humility.',
     group: 'anger'
 },
     {
     attributeName: 'immutable',
     emotionName: 'Insecurity',
-    emotionDescription: 'Being open to danger or threat; lack of protection',
+    emotionConnectDescription: 'Insecurity comes from threats of change to what I hold dear - but God is Immutable and never changes.',
     group: 'fear'
 },
     {
     attributeName: 'incomprehensible',
     emotionName: 'Doubt',
-    emotionDescription: 'Indecision between belief and disbelief',
+    emotionConnectDescription: 'Doubt lies between belief and disbelief - and is a very natural byproduct of an Incomprehensible God.',
     group: 'anger'
 },
     {
     attributeName: 'independent',
     emotionName: 'Contentment',
-    emotionDescription: 'Happiness and satisfaction with what you already have',
+    emotionConnectDescription: 'Contentment is happiness with what I already have - and a fully Independent God can afford to give us contentment in Him.',
     group: 'pleasure'
 },
     {
     attributeName: 'invisible',
     emotionName: 'Distress',
-    emotionDescription: 'When something bad is happening to you right now and you need help',
+    emotionConnectDescription: 'Distress is an urgent need for tangible help - and an Invisible God will be seen only by those with faith in Him.',
     group: 'fear'
 },
     {
     attributeName: 'jealous',
     emotionName: 'Fear',
-    emotionDescription: 'Unpleasant emotion from anticipation or awareness of danger',
+    emotionConnectDescription: 'Fear anticipates danger to what I love - and a Jealous God should be both feared and loved.',
     group: 'fear'
 },
     {
     attributeName: 'joyful',
     emotionName: 'Happiness',
-    emotionDescription: 'A feeling of pleasure, delight or gladness',
+    emotionConnectDescription: 'Happiness is a feeling of pleasure and delight - and God is the source of all Joy.',
     group: 'pleasure'
 },
     {
     attributeName: 'just',
     emotionName: 'Indignation',
-    emotionDescription: 'Anger provoked by what is perceived as unfair treatment',
+    emotionConnectDescription: 'Indignation makes me want to correct unfair treatment - but God is Just, not necessarily fair.',
     group: 'anger'
 },
     {
     attributeName: 'loving',
     emotionName: 'Shame',
-    emotionDescription: 'The painful feeling of believing we are unworthy of love and belonging',
+    emotionConnectDescription: 'Shame tells me I am unworthy of love or belonging - but God is Loving to all.',
     group: 'sadness'
 },
     {
     attributeName: 'merciful',
     emotionName: 'Resentment',
-    emotionDescription: 'Smoldering anger due to real or imagined offenses or unfairness',
+    emotionConnectDescription: 'Resentment clings to an anger over real or imagined offenses - but God is Merciful to those unworthy.',
     group: 'anger'
     },
     {
     attributeName: 'omnipotent',
     emotionName: 'Nervousness',
-    emotionDescription: 'Out-of-control feeling from thinking something might go wrong',
+    emotionConnectDescription: 'Nervousness is being powerless in view of all that could go wrong - but God is lovingly Omnipotent.',
     group: 'fear'
     },
     {
     attributeName: 'omnipresent',
     emotionName: 'Loneliness',
-    emotionDescription: 'Sadness in response to perceived isolation',
+    emotionConnectDescription: 'Loneliness is sadness in response to perceived isolation - but God is Omnipresent.',
     group: 'sadness'},
     {
     attributeName: 'omniscient',
     emotionName: 'Misunderstood',
-    emotionDescription: 'Feeling incorrectly interpreted or understood',
+    emotionConnectDescription: 'I feel misunderstood when others see me incorrectly - but God is Omniscent.',
     group: 'fear'},
     {
     attributeName: 'patient',
     emotionName: 'Self Pity',
-    emotionDescription: 'Excessive, self-absorbed unhappiness over my own troubles',
+    emotionConnectDescription: 'Self-pity is excessive, self-absorbed unhappiness over my own troubles - but God gives Patience in suffering.',
     group: 'sadness'},
     {
     attributeName: 'patient',
     emotionName: 'Annoyance',
-    emotionDescription: 'Being annoyed or irritated',
+    emotionConnectDescription: 'Annoyance blanes another for my suffering - but God gives Patience in suffering.',
     group: 'anger'},
     {
     attributeName: 'peaceful',
     emotionName: 'Dissatisfaction',
-    emotionDescription: 'Unfulfillment of expections',
+    emotionConnectDescription: 'Dissatisfaction stems from unfulfilled expectations - but God brings Peace.',
     group: 'sadness'},
     {
     attributeName: 'planning',
     emotionName: 'Worry',
-    emotionDescription: 'Ongoing uneasiness from dwelling on a real or perceived problem',
+    emotionConnectDescription: 'Worry is ongoing uneasiness over what could go wrong - but God is a perfect Planner.',
     group: 'fear'},
     {
     attributeName: 'providing',
     emotionName: 'Pride',
-    emotionDescription: 'Deep pleasure or satisfaction derived from my own achievements',
+    emotionConnectDescription: 'Pride is deep satisfaction in my own achievements - but it is God who Provides.',
     group: 'happiness'},
     {
     attributeName: 'relational',
     emotionName: 'Frustration',
-    emotionDescription: 'When my persistent efforts to achieve are being blocked',
+    emotionConnectDescription: 'Frustration comes when my persistent efforts appear blocked by others - but God embraces Relationship.',
     group: 'anger'},
     {
     attributeName: 'saving',
     emotionName: 'Desperation',
-    emotionDescription: 'Losing almost all hope, but willing to do anything to achieve a goal',
+    emotionConnectDescription: 'Desperation sees hope draining even with a willingness to do anything  - but only God can truly Save.',
     group: 'anger'},
     {
     attributeName: 'sovereign',
     emotionName: 'Sadness',
-    emotionDescription: 'The urge to withdraw and to seek comfort after a loss',
+    emotionConnectDescription: 'Sadness urges me to withdraw and seek comfort after a loss - but what I need is my Sovereign and Lord.',
     group: 'sadness'},
     {
     attributeName: 'teaching',
     emotionName: 'Reluctance',
-    emotionDescription: 'Not wanting to do something you should do',
+    emotionConnectDescription: 'Reluctance keeps me from doing what I know I should - but God is a faithful Teacher.',
     group: 'fear'},
     {
     attributeName: 'truthful',
     emotionName: 'Distrust',
-    emotionDescription: 'Being careful around a person who is not truthful or has poor intent',
+    emotionConnectDescription: 'Distrust is being careful around those who may have poor intent - but God is Truth, which builds trust.',
     group: 'fear'},
     {
     attributeName: 'unique',
     emotionName: 'Humiliation',
-    emotionDescription: 'Pain from deliberate harm to your reputation',
+    emotionConnectDescription: 'Humiliation may deliberately cause pain to my reputation - but it changes not how a Unique God sees me.',
     group: 'sadness'},
     {
     attributeName: 'wise',
     emotionName: 'Regret',
-    emotionDescription: 'Sadness or disappointment coming after an earlier decision or action',
+    emotionConnectDescription: 'Regret is sadness that follows an earlier action - but God is Wise and gives Wisdom to go forward.',
     group: 'fear'},
     {
     attributeName: 'wrathful',
     emotionName: 'Contempt',
-    emotionDescription: 'Looking down on someone and not wanting anything to do with them',
+    emotionConnectDescription: 'Contempt looks down on another and is driven by hate - but God is Wrathful, which is driven by love.',
     group: 'anger'}
 ]
 
