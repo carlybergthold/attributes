7e752c4589a0d5e668f368c211f42ac62f465231
bergthold <67835297+bergthold@users.noreply.github.com>
2021-08-01 12:11:35 -0500

    Update omnipresent.jsx

--- src/modules/attributeDetail/omnipresent.jsx
+++ src/modules/attributeDetail/omnipresent.jsx
@@ -29,15 +29,15 @@ class Omnipresent extends Component {
                   <h1 className="title tracking-wide padded" style={{color: styleMethods.getAttributeColor("omnipresent")}}>Omnipresent means to exist everywhere.
                   </h1></section>
                   <p className="is-size-6 leading-normal padded"> We often desire to be in more than one place at one time.  We try to at least optimize what time and presence we have, yet still experience the much dreaded FOMO (Fear Of Missing Out).</p>
-                  <p className="is-size-6 leading-normal padded">The desire to extend our experiences can take many forms - simple calendar optimization (often resulting in over-scheduling), social media experience sharing, skyping, real or virtual assistants.  All these can "extend" our physical presence and maximize our experience of the world. </p>
-                  <p className="is-size-6 leading-normal padded">I can experience loneliness even with others present.  How much I interact with those around me matters a lot - and how much I want to interact with those NOT around me can matter even more.  It may really seem, at times, that God is not with me - but He is.</p>
+                  <p className="is-size-6 leading-normal padded">The desire to extend our experiences can take many forms - simple calendar optimization (often resulting in over-scheduling), social media sharing, video conferencing, real or virtual assistants.  All these can "extend" our physical presence and maximize our experience of the world. </p>
+                  <p className="is-size-6 leading-normal padded">But I can experience loneliness even with others present.  How much I interact with those around me matters a lot, and how much I want to interact with those NOT around me can matter even more.  At times it may even seem that God is not with me - but He is. Always.</p>
                 </div>
 
                 <div className="attSection" id="emotion">
                 <section>
                   <h1 className="title tracking-wide padded" style={{color: styleMethods.getAttributeColor("omnipresent")}}>I can feel lonely even while I am with other people.</h1>
                   </section>
-                  <p className="is-size-6 leading-normal padded">A short-term spell of being alone can encourage me to strengthen the depth and breadth of my relationships, but long-term loneliness has been proven to be hazardous to both physical and mental health. The psalmist below presents a believer’s perspective - that God’s continuing presence provides positive guidance and security.  And this guidance always encourages me to love other people, which is extremely helpful to combat loneliness.</p>
+                  <p className="is-size-6 leading-normal padded">A short-term spell of being alone can encourage me to strengthen the depth and breadth of my relationships, but long-term loneliness has been proven to be hazardous to both physical and mental health. A CDC survey several months into the COVID pandemic reported that >40% of respondents had symptoms of anxiety or depression, with 26% indicating that they had started or increased substance use as a way to cope. The psalmist below presents a believer’s perspective - that God’s continuing presence provides positive guidance, companionship and security.  And this guidance always encourages me to love other people, which is an extremely helpful practice to combat loneliness.</p>
                   <br></br>
                   <Quote scripture="Where shall I go from your Spirit? Or where shall I flee from your presence? If I ascend to heaven, you are there! If I make my bed in Sheol, you are there! If I take the wings of the morning and dwell in the uttermost parts of the sea, even there your hand shall lead me, and your right hand shall hold me. If I say, ‘Surely the darkness shall cover me, and the light about me be night,’ even the darkness is not dark to you; the night is bright as the day, for darkness is as light with you." reference="Psalm 139:7-12" color="var(--teal)"/>
                 </div>
@@ -46,8 +46,8 @@ class Omnipresent extends Component {
                 <section>
                   <h1 className="title tracking-wide padded" style={{color: styleMethods.getAttributeColor("omnipresent")}}>How do I seek JoMO versus FoMO?</h1>
                 </section>
-                  <p className="is-size-6 leading-normal padded">FoMO (Fear of Missing Out) was first identified by a marketing strategist in 1996 and is "a pervasive apprehension that others might be having rewarding experiences from which one is absent". This social anxiety is characterized by "a desire to stay continually connected with what others are doing".   Wikipedia</p>
-                  <p className="is-size-6 leading-normal padded">FoMO's opposite, JoMO (Joy of Missing Out), is defined by the Urban Dictionary as "You’re enjoying what you’re doing in the here and now and not on social media broadcasting or seeing what everybody else is doing."</p>
+                  <p className="is-size-6 leading-normal padded">FoMO (Fear of Missing Out) was first identified by a marketing strategist in 1996 and is a pervasive apprehension that others might be having rewarding experiences from which one is absent. This social anxiety is characterized by a desire to stay continually connected with what others are doing.   (Wikipedia)</p>
+                  <p className="is-size-6 leading-normal padded">FoMO's opposite, JoMO (Joy of Missing Out), is defined by the Urban Dictionary as, "You’re enjoying what you’re doing in the here and now and not on social media broadcasting or seeing what everybody else is doing."</p>
                   <p className="is-size-6 leading-normal padded">In Ecclesiastes, Solomon concludes that life is a gift from God with which to serve God and do good - and to actively seek joy even in what we may consider toil.  He does not tell me to seek joy in what I choose not to do or am not able to participate in.</p>
                   <br></br>
                   <Quote scripture="Behold, what I have seen to be good and fitting is to eat and drink and find enjoyment in all the toil with which one toils under the sun the few days of his life that God has given him, for this is his lot." reference="Ecclesiastes 5:18" color="var(--teal)"/>
@@ -57,7 +57,7 @@ class Omnipresent extends Component {
                 <section>
                   <h1 className="title tracking-wide padded" style={{color: styleMethods.getAttributeColor("omnipresent")}}>It seems God is not with me right now.</h1>
                   </section>
-                  <p className="is-size-6 leading-normal padded">In the midst of suffering, pain, sickness, sorrow, warfare, and other natural disasters, it is hard to perceive a loving and merciful God simply watching it all go down.  I can deal with this apparent disconnect by denying His presence or by trusting that God is present and has a reason for allowing the difficulty.</p>
+                  <p className="is-size-6 leading-normal padded">In the midst of suffering, pain, sickness, sorrow, warfare, and natural disasters, it is hard to perceive a loving and merciful God simply watching it all go down.  I can deal with this apparent disconnect either by denying His presence or by trusting that God is present and has a good reason for allowing the difficulty.</p>
                   <br></br>
 
                   <Quote scripture=" 'Am I a God at hand,' declares the LORD, 'and not a God far away? Can a man hide himself in secret places so that I cannot see him?' declares the LORD. 'Do I not fill heaven and earth?' declares the LORD." reference="Jeremiah 23:23-24" color="var(--teal)"/>
@@ -66,7 +66,7 @@ class Omnipresent extends Component {
                 <div className="attSection" id="whoAmI">
                 <section>
                   <h1 className="title tracking-wide padded" style={{color: styleMethods.getAttributeColor("omnipresent")}}>Only God is omnipresent.</h1></section>
-                  <p className="is-size-6 leading-normal padded">God is not the substance of things, but is transcendent over all things and is separate from His creation. Humans are not, and never will be, omnipresent.  Some classify attributes such as omnipresence as God's incommunicable attributes, meaning it will never be imparted on humans.  But having His image means I can still feel it in my desire.  Omnipresence does not mean God is everywhere in the same manner.</p>
+                  <p className="is-size-6 leading-normal padded">God is not the substance of things, but is transcendent over all things and is separate from His creation. Humans are not, and never will be, omnipresent.  Some classify attributes such as omnipresence as God's incommunicable attributes, meaning it will never be imparted on humans.  But having His image means I can still feel it in my desire.  Omnipresence does not mean we perceive God everywhere in the same manner.</p>
                   <br></br>
                   <Quote scripture="God is in heaven in regard of the manifestation of glory, in hell by the expression of his justice, in the earth by the discoveries of his wisdom, power, patience, and compassion, in his people by the monuments of his grace, and in all in regard of his substance." reference="Stephen Charnock" color="var(--teal)"/>
                 </div>
@@ -74,10 +74,10 @@ class Omnipresent extends Component {
                 <div className="attSection" id="biblicalChristianity">
                   <section>
                   <h1 className="title tracking-wide padded" style={{color: styleMethods.getAttributeColor("omnipresent")}}>Jesus is also omnipresent.</h1></section>
-                  <p className="is-size-6 leading-normal padded">Jesus, as God, shares all of God’s attributes.  Jesus willingly limited Himself to his physical body while on earth, but then affirmed His omnipresence for eternity. His blood is the means by which I am brought near to God.  God can be very close to me while I am yet far from Him.</p>
+                  <p className="is-size-6 leading-normal padded">Jesus, as God, shares all of God’s attributes.  Jesus willingly limited Himself to his physical body while on earth, but then affirmed His omnipresence for eternity. His blood is the means by which God brings me near to Him. </p>
 
                   <Quote scripture="And behold, I (Jesus speaking) am with you always, to the end of the age." reference="Matthew 28:20" color="var(--teal)"/>
-                  <Quote scripture="But now in Christ Jesus you who once were far off have been brought near by the blood of Christ." reference="Ephesians 2:13" color="var(--teal)"/>
+                  <Quote scripture="But now in Christ Jesus you who once were far off have been brought near (to God) by the blood of Christ." reference="Ephesians 2:13" color="var(--teal)"/>
                 </div>
 
                 <div className="attSection" id="soWhat">
@@ -92,7 +92,7 @@ class Omnipresent extends Component {
                   <section>
                   <h1 className="title tracking-wide padded" style={{color: styleMethods.getAttributeColor("omnipresent")}}>Prayer</h1>
                   </section>
-                  <p className="is-size-6 leading-normal padded">God, You are with me always.  Remind me of this when I feel like you are not present.  Help me to feel Your image in me, as well as Your Spirit.</p>
+                  <p className="is-size-6 leading-normal padded">God, You are with me always.  Remind me of this when I feel like you are not present.  Help me to be aware of Your image in me, as well as Your Spirit.</p>
                   <Quote scripture="Draw near to God, and he will draw near to you." reference="James 4:8a" color="var(--teal)"/>
                 </div>
 
@@ -102,15 +102,14 @@ class Omnipresent extends Component {
                   </section>
                   <p className="is-size-6 leading-normal padded">We’ve known for some time now that electrons can exist in more than one spot, so when will humans be able to do that?  Current research recently expanded the size and caused a 2,000-atom molecule to be two places at once.  No, it is not considered likely that humans will ever be able to exist in more than one location, but it should open the mind of an unbelieving physicist to the potential for an omnipresent God.</p>
                   <p className="is-size-6 leading-normal padded">“About 80 years ago, scientists discovered that it is possible to be in two locations at the same time—at least for an atom or a subatomic particle, such as an electron. For such tiny objects, the world is governed by a madhouse set of physical laws known as quantum mechanics. At that size range, every bit of matter and energy exists in a state of blurry flux, allowing it to occupy not just two locations but an infinite number of them simultaneously.” - Discover magazine, June 2005</p>
-                  <p className="is-size-6 leading-normal padded">“Giant molecules can be in two places at once, thanks to quantum physics. That's something that scientists have long known is theoretically true based on a few facts: Every particle or group of particles in the universe is also a wave — even large particles, even bacteria, even human beings, even planets and stars. And waves occupy multiple places in space at once. So any chunk of matter can also occupy two places at once. Physicists call this phenomenon "quantum superposition," and for decades, they have demonstrated it using small particles.  But in recent years, physicists have scaled up their experiments, demonstrating quantum superposition using larger and larger particles. Now, in a paper published Sept. 23 in the journal Nature Physics, an international team of researchers has caused molecule made up of up to 2,000 atoms to occupy two places at the same time.” - 10/6/19 article in “space”.com</p>
                 </div>
 
                 <div className="attSection" id="history">
                 <section>
                   <h1 className="title tracking-wide padded" style={{color: styleMethods.getAttributeColor("omnipresent")}}>How has God revealed His Presence throughout history?</h1>
                   </section>
-                  <p className="is-size-6 leading-normal padded">Although God exists everywhere at all times, a clear understanding of who He is has not.  Even after Jesus was physically revealed on earth, He chose the means of further spreading this gospel message to be the Holy Spirit and the Bible, working together through faithful believers to bring this understanding to those who will receive it.</p>
-                  <p className="is-size-6 leading-normal padded">Beginning with Pentecost, the Holy Spirit used the vastness of Rome to play a key role in widely dispersing the apostles and early believers' message - first through persecution, and then by formal adoption of the clear gospel message (Constantine in 313), resulting in the Nicene and Athanasian creeds and formal Biblical canon (all in 300s). But as Rome fell, the light was again dispersed and carried through the Dark and Medieval years by monastic pockets of believers (notably St. Patrick in Ireland), faithfully tending to the written scriptures, even if not in practice. It was then the printing press physically spread the Word, resulting in the Reformation. From there, the gospel message has permeated the entire globe.</p>
+                  <p className="is-size-6 leading-normal padded">Although God exists everywhere at all times, a clear understanding of who He is has not.  Even after Jesus physically revealed Himself on earth, His Presence was continually revealed through the Bible and Holy Spirit, in partnership with very flawed, yet faithful, believers ever since.</p>
+                  <p className="is-size-6 leading-normal padded">Beginning with Pentecost, the Holy Spirit used the vastness of Rome to play a key role in widely dispersing the apostles and early believers and their message - first through persecution, and then by formal adoption of the clear gospel message (Constantine in 313), resulting in the Nicene and Athanasian creeds and formal Biblical canon (all in 300s). But as Rome fell, the message of light was again dispersed and carried through the Dark and Medieval years by monastic pockets of believers (notably St. Patrick in Ireland), faithfully tending to the written scriptures, even if not in practice. Later the printing press physically spread the Word, resulting in the Reformation. From there, the gospel message has been revealed throughout the entire globe.</p>
                 </div>
 
                 <div className="attSection" id="liturgy" >
